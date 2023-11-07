25bab6b6dcf3e4c689fd281434bb22a722570a78
Josh Coupar-Evans <joshuacoups03@gmail.com>
2023-11-07 14:39:00 +0000

    practical :)

--- plot_data.R
+++ plot_data.R
@@ -1,11 +1,12 @@
 #Script to plot the logistic growth data
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment1.csv")
 
 install.packages("ggplot2")
 library(ggplot2)
 
-ggplot(aes(t,N), data = ???) +
+#plotting number against time on X
+ggplot(aes(t,N), data = growth_data) +
   
   geom_point() +
   
@@ -15,7 +16,8 @@ ggplot(aes(t,N), data = ???) +
   
   theme_bw()
 
-ggplot(aes(t,???), data = growth_data) +
+#log scale
+ggplot(aes(t,N), data = growth_data) +
   
   geom_point() +
   
